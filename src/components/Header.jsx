de337c3f29520a775f5967756a00bd702b1b02d2
AlexS997 <alexandru.solomon@yahoo.com>
2025-08-07 08:29:34 +0200

    First Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,76 @@
+import NavButton from './NavButton'
+import Logo from '/src/assets/images/logo.png'
+import 'boxicons/css/boxicons.min.css'
+
+const Header = () => {
+
+    const toggleMenu = () => {
+        const mobileMenu = document.getElementById("menu")
+
+        if(mobileMenu.classList.contains("hidden")){
+            mobileMenu.classList.remove('hidden')
+        }else{
+            mobileMenu.classList.add('hidden')
+        }
+    }
+
+  return (
+    <header className='py-1 px-7 flex justify-between items-center sticky top-0 z-50 w-full border-b-[0.3px] border-[#babaff]'>
+        <div className='flex lg:gap-14 gap-4 items-center'>
+            <img src={Logo} alt="logo" 
+                className='md:w-16 w-12'
+            />
+
+            <div className='hidden md:flex gap-5 items-center'>
+                <button className='h-8 px-6 bg-gradient-to-r from-purple-500 to-indigo-600 rounded-lg font-medium text-nowrap hover:opacity-70 transition-all duration-300 cursor-pointer'>
+                    PLAY NOW
+                </button>
+
+                <button className='h-8 px-6 bg-gradient-to-r from-gray-600 to-gray-400 rounded-lg font-medium text-nowrap hover:opacity-70 transition-all duration-300 cursor-pointer'>
+                    PRODUCTS
+                </button>
+            </div>
+        </div>
+
+        <nav className='hidden md:flex lg:gap-8 gap-4'>
+
+            <NavButton icon={'bx bx-user-circle'} text={'Avatar'} />
+            <NavButton icon={'bx bxs-diamond'} text={'Arena'} />
+            <NavButton icon={'bx bx-chevrons-up'} text={'Beyond'} />
+            <NavButton icon={'bx bxs-store'} text={'Shop'} />
+
+        </nav>
+
+        {/* Mobile Menu - visible mobile*/}
+        <button onClick={toggleMenu}  className='text-3xl p-2 md:hidden cursor-pointer'>
+            <i class='bx bx-menu'></i>
+        </button>
+
+        {/* Mobile Menu - default hidden*/}
+        <div id='menu'
+        className='hidden fixed top-18.5 right-0 left-0 p-5 md:hidden'
+        style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+        >
+            <nav className='flex flex-col gap-4 items-center'>
+                <NavButton icon={'bx bx-user-circle'} text={'Avatar'} />
+                <NavButton icon={'bx bxs-diamond'} text={'Arena'} />
+                <NavButton icon={'bx bx-chevrons-up'} text={'Beyond'} />
+                <NavButton icon={'bx bxs-store'} text={'Shop'} />
+            </nav>
+
+            <div className='flex flex-col gap-3 w-full mt-4'>
+                <button className='bg-purple-700 py-2 rounded cursor-pointer'>
+                    Play Now!
+                </button>
+                
+                <button className='bg-gray-500 py-2 rounded cursor-pointer'>
+                    NFT Store
+                </button>
+            </div>
+        </div>
+
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Hero.jsx
@@ -0,0 +1,47 @@
+import Video from '/src/assets/videos/hero.mp4'
+import Img1 from '/src/assets/images/illu-text.png'
+import Img2 from '/src/assets/images/illu-logo.png'
+
+const Hero = () => {
+  return (
+    <main className="relative w-full h-screen overflow-hidden flex justify-center mb-[10%]">
+        <video src={Video}
+            autoPlay
+            loop
+            muted
+            playsInline
+            className='w-full h-[95%] object-cover absolute top-0 first-letter:left-0 -z-10'
+        ></video>
+
+        <div className='absolute bottom-[15%] flex flex-col items-center gap-5'>
+            <img src={Img1} alt="img1" 
+                className='md:w-[30rem] w-[20rem]'
+            />
+
+            <h1 className='md:text-2xl text-xl font-bold'>
+                Explore, Capture, Conquer
+            </h1>
+
+            <div className='md:w-[75%] w-[60%] h-[0.1px] bg-[#baba]'>
+            </div>
+
+            <button className='h-10 px-16 bg-gradient-to-r from-purple-500 to-indigo-600 rounded-lg font-medium text-nowrap hover:opacity-70 transition-all duration-300 cursor-pointer'>
+                PLAY NOW
+            </button>
+
+            <div className='flex items-center gap-5 text-3xl font-extrabold text-gray-200'>
+                <img src={Img2} alt="img2" 
+                    className='md:h-16 h-12'
+                />
+                ZERO
+            </div>
+
+            <p className=''>
+                Notice: Illuvium Games are in Beta. Participation involves risk. Read our full Disclaimer here.
+            </p>
+        </div>
+    </main>
+  )
+}
+
+export default Hero
